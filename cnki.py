ed1240629b0909c83398314ff5177fa68938b85b
Wolfson <wolfsonliu@gmail.com>
2018-04-18 10:50:54 +0800

    handle checkcode

--- cnki.py
+++ cnki.py
@@ -5,7 +5,9 @@ import string
 import random
 import pandas as pd
 import subprocess as sp
+from io import StringIO
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from PIL import Image
 from PIL import ImageEnhance
 
@@ -35,41 +37,106 @@ def orc(img, tesseract=os.path.join(os.getcwd(), 'tesseract-4.0.0-alpha/tesserac
 
 
 ff = webdriver.Firefox(executable_path=r'./geckodriver.exe')
-drive = ff
+driver = ff
 year = 2011
+collage_name = '北京大学'
 
-drive.get('http://kns.cnki.net/kns/brief/result.aspx?dbprefix=CJFQ')
+driver.get('http://kns.cnki.net/kns/brief/result.aspx?dbprefix=CJFQ')
+driver.maximize_window()
 
-def get_search_result_number(drive):
-    drive.switch_to_frame('iframeResult')
-    pagenumberspan = drive.find_element_by_class_name('pagerTitleCell')
+def get_search_result_number(driver):
+    # 返回总搜索结果
+    driver.switch_to.default_content()
+    driver.switch_to_frame('iframeResult')
+    pagenumberspan = driver.find_element_by_class_name('pagerTitleCell')
     pn = pagenumberspan.get_attribute('innerHTML').split('&nbsp')[2].replace(';', '').replace(',', '')
-    drive.switch_to.default_content()
+    driver.switch_to.default_content()
     return pn
 
-def get_info(drive):
+
+def get_info(driver, header=''):
+    # 下载页面显示的表格结果
     timere = re.compile('[0-9]{4}-[0-9]{1,2}-[0-9]{1,2} [0-9]{1,2}:[0-9]{1,2}')
-    drive.switch_to_frame('iframeResult')
-    trs = drive.find_element_by_class_name('GridTableContent').text.split('\n').find_elements_by_tag_name('tr')
+    driver.switch_to.default_content()
+    driver.switch_to_frame('iframeResult')
+    trs = driver.find_element_by_class_name('GridTableContent').find_elements_by_tag_name('tr')
     tabletext = list()
-    tabletext[0] = ' '.join(['序号', trs[0].text]).replace(' ', '\t')
-    drive.switch_to.default_content()
+    tabletext.append( ' '.join(['序号', trs[0].text]).replace(' ', '\t'))
+    for x in trs[1:]:
+        text = x.text
+        times = timere.findall(text)  # 找到 2017-08-14 14:08 时间中的空格并替换为下划线
+        if len(times) > 0:
+            for tx in times:
+                text = text.replace(tx, tx.replace(' ', '_'))
+        text = text.replace('; ', ';')
+        tabletext.append(text.replace(' ', '\t'))  # 把空格替换为 \t
+    driver.switch_to.default_content()
+    return tabletext
+
+
+def nextpage(driver):
+    # 搜索结果的下一页，如果有下一页则跳转到下一页，并返回 True，如果没有下一页则返回 False
+    driver.switch_to.default_content()
+    driver.switch_to_frame('iframeResult')
+    driver.execute_script('document.body.scrollIntoView()')  ## 滚动屏幕到按钮所在位置
+    try:
+        next_page = driver.find_element_by_id('Page_next')
+        next_page.click()
+    except NoSuchElementException:
+        next_page = None
+    finally:
+        driver.switch_to.default_content()
+        driver.execute_script('document.body.scrollIntoView()')  ## 滚动屏幕到最上层
+    if next_page is None:
+        return False
+    else:
+        return True
+
+
+def checkcode(driver):
+    driver.switch_to.default_content()
+    driver.switch_to_frame('iframeResult')
+    driver.execute_script('document.body.scrollIntoView()')  ## 滚动屏幕到按钮所在位置
+    try:
+        driver.find_element_by_id('CheckCodeImg')
+    except NoSuchElementException:
+        driver.switch_to.default_content()
+        driver.execute_script('document.body.scrollIntoView()')
+        return False
+    try:
+        while driver.find_element_by_tag_name('label').text == '请输入验证码：':
+            checkcode_img = driver.find_element_by_id('CheckCodeImg')
+            checkcode_img.click()
+            checkcode_img.screenshot('checkcode.png')
+            captcha = orc('checkcode.png')
+            time.sleep(random.randint(1,3))
+            captcha_entry = driver.find_element_by_id('CheckCode')
+            driver.execute_script('arguments[0].value = arguments[1]', captcha_entry, captcha)
+            captch_submit = driver.find_elements_by_tag_name('input')[1]
+            captch_submit.click()
+    except NoSuchElementException:
+        driver.switch_to.default_content()
+        driver.execute_script('document.body.scrollIntoView()')  ## 滚动屏幕到最上层
+        return True
+
+
+
 
 # 检索按钮
-searchbtn = drive.find_element_by_id('btnSearch')
+searchbtn = driver.find_element_by_id('btnSearch')
 
 # 选择学校
-collage = drive.find_element_by_id('au_1_value2')
-drive.execute_script('arguments[0].value = arguments[1]', collage, '北京大学')
+collage_input = driver.find_element_by_id('au_1_value2')
+driver.execute_script('arguments[0].value = arguments[1]', collage_input, collage_name)
 
 # 选择年份
-yearfrom = drive.find_element_by_name('year_from')
+yearfrom = driver.find_element_by_name('year_from')
 yearfrom.send_keys('{0}'.format(year))
-yearto = drive.find_element_by_name('year_to')
+yearto = driver.find_element_by_name('year_to')
 yearto.send_keys('{0}'.format(year))
 
 # 选择来源
-journalsource = drive.find_element_by_id('ddSubmit').find_elements_by_class_name('dd01')[3]
+journalsource = driver.find_element_by_id('ddSubmit').find_elements_by_class_name('dd01')[3]
 jsall = journalsource.find_element_by_id('AllmediaBox')
 if jsall.is_selected():
     jsall.click()
@@ -78,11 +145,11 @@ jsbox[3].click()
 jsbox[4].click()
 
 # 选择类别
-clear_btn = drive.find_element_by_id('XuekeNavi_Div').find_elements_by_class_name('btn')[0]
+clear_btn = driver.find_element_by_id('XuekeNavi_Div').find_elements_by_class_name('btn')[0]
 clear_btn.click()
-category = drive.find_element_by_id('XuekeNavi_Div').find_element_by_id('A').find_element_by_id('Afirst')
+category = driver.find_element_by_id('XuekeNavi_Div').find_element_by_id('A').find_element_by_id('Afirst')
 category.click()
-subcategorydl = drive.find_element_by_id('XuekeNavi_Div').find_element_by_id('Achild')
+subcategorydl = driver.find_element_by_id('XuekeNavi_Div').find_element_by_id('Achild')
 
 for subid in ['A' + '{0:03d}'.format(x) for x in range(1, 14)]:
     subcategorydl.find_element_by_id(subid + 'first').click()  # open subsubcategory list
@@ -90,6 +157,20 @@ for subid in ['A' + '{0:03d}'.format(x) for x in range(1, 14)]:
     for subsub in subsubcategorys:
         subsubid = subsub.get_property('value')
         subsub.click()
+        searchbtn.click()  # 搜索
+        if int(get_search_result_number(driver)) > 3000:
+            raise ValueError('搜索结果超出限额：' + get_search_result_number(driver))
+        while True:
+            if
+            tabletext = get_info(driver)
+            tabletext[0] = '科目\t' + tabletext[0]
+            for row in range(1, len(tabletext)):
+                tabletext[row] = '\t'.join([subsubid, tabletext[row]])
+            with open('_'.join([collage_name, year, subsubid]), 'ab') as f:
+                f.write('\n'.join(tabletext[1:]).encode('utf-8'))
+                f.write('\n'.encode('utf-8'))
+            if not nextpage(driver):
+                break
 
         subsub.click()
     subcategorydl.find_element_by_id(subid + 'second').click()  # close subsubcategory list
