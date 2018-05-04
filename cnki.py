b0c9c232301fea1c939b08b5c7eb6f8cc27d4da3
Wolfson <wolfsonliu@gmail.com>
2018-05-04 09:17:12 +0800

    download xls rather than txt

--- cnki.py
+++ cnki.py
@@ -229,13 +229,13 @@ def crawler(collage_name, year_start, year_end, subject_id_list):
     driver.execute_script('arguments[0].value = arguments[1]', collage_input, collage_name)
 
     # 选择年份
-    time.sleep(1)
+    time.sleep(2)
     yearfrom = driver.find_element_by_name('year_from')
     yearfrom.send_keys('{0}'.format(year_start))
-    time.sleep(1)
+    time.sleep(2)
     yearto = driver.find_element_by_name('year_to')
     yearto.send_keys('{0}'.format(year_end))
-    time.sleep(1)
+    time.sleep(2)
 
     # 选择来源
     journalsource = driver.find_element_by_id('ddSubmit').find_elements_by_class_name('dd01')[3]
@@ -245,7 +245,7 @@ def crawler(collage_name, year_start, year_end, subject_id_list):
     jsbox = journalsource.find_elements_by_tag_name('input')[2:]
     jsbox[3].click()
     jsbox[4].click()
-    time.sleep(1)
+    time.sleep(2)
 
     # 选择类别
     clear_btn = driver.find_element_by_id('XuekeNavi_Div').find_elements_by_class_name('btn')[0]
@@ -255,9 +255,10 @@ def crawler(collage_name, year_start, year_end, subject_id_list):
     time.sleep(1)
 
     for subid in subject_id_list:
-        subcategorydlfirst = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.ID, subid + 'first'))
-        )
+        # subcategorydlfirst = WebDriverWait(driver, 6).until(
+        #     EC.presence_of_element_located((By.ID, subid + 'first'))
+        # )
+        subcategorydlfirst = driver.find_element_by_id(subid + 'first')
         subcategorydlfirst.click()  # open subsubcategory list
         time.sleep(1)
         subsubcategorys = driver.find_element_by_id(subid + 'child').find_elements_by_id('selectbox')
@@ -323,10 +324,62 @@ def crawler(collage_name, year_start, year_end, subject_id_list):
 
 
 
+
+
+###########################################
+
+
+subject_name = {
+    'A001': '自然科学理论与方法', 'A002': '数学', 'A003': '非线性科学与系统科学', 'A004': '力学',
+    'A005': '物理学', 'A006': '生物学', 'A007': '天文学', 'A008': '自然地理学和测绘学', 'A009': '气象学',
+    'A010': '海洋学', 'A011': '地质学', 'A012': '地球物理学', 'A013': '资源科学'
+}
+subsubject_name = {
+    'A001_1': '自然科学研究的方针政策', 'A001_2': '自然科学理论与方法论', 'A001_3': '自然科学现状及发展',
+    'A001_4': '自然科学研究及自然科学史',
+    'A002_1': '数学概论', 'A002_2': '古典数学', 'A002_3': '初、高等数学',
+    'A002_4': '数理逻辑、数学基础', 'A002_5': '代数、数论、组合理论', 'A002_6': '数学分析',
+    'A002_7': '几何、拓扑', 'A002_8': '动力系统理论', 'A002_9': '概率论 、数理统计', 'A002_A': '运筹学',
+    'A002_B': '控制论、信息论(数学理论)', 'A002_C': '计算数学', 'A002_D': '应用数学',
+    'A003_1': '非线性科学', 'A003_2': '系统科学',
+    'A004_1': '力学概论', 'A004_2': '理论力学(一般力学)', 'A004_3': '振动理论', 'A004_4': '连续介质力学(变形体力学)',
+    'A004_5': '固体力学', 'A004_6': '流体力学', 'A004_7': '流变学', 'A004_8': '爆炸力学', 'A004_9': '应用力学',
+    'A005_1': '物理学总论', 'A005_2': '物理学现状、概况', 'A005_3': '物理学实验方法与设备', 'A005_4': '物理测量',
+    'A005_5': '理论物理学', 'A005_6': '声学', 'A005_7': '光学', 'A005_8': '电磁学、电动力学', 'A005_9': '无线电物理学',
+    'A005_A': '真空电子学(电子物理学)', 'A005_B': '半导体物理学', 'A005_C': '固体物理学', 'A005_D': '低温物理学',
+    'A005_E': '高压与高温物理学', 'A005_F': '等离子体物理学', 'A005_G': '热学与物质分子运动论',
+    'A005_H': '分子物理学、原子物理学', 'A005_I': '原子核物理学、高能物理学', 'A005_J': '应用物理学',
+    'A006_1': '生物科学总论', 'A006_2': '普通生物学', 'A006_3': '细胞学', 'A006_4': '遗传学', 'A006_5': '生理学',
+    'A006_6': '生物化学', 'A006_7': '生物物理学', 'A006_8': '分子生物学及遗传工程', 'A006_9': '生物工程学',
+    'A006_A': '环境生物学', 'A006_B': '古生物学', 'A006_C': '微生物学理论及其应用', 'A006_D': '植物学',
+    'A006_E': '动物学', 'A006_F': '昆虫学', 'A006_G': '人类学',
+    'A007_1': '一般性问题', 'A007_2': '天文观测设备与观测资料', 'A007_3': '天体测量学', 'A007_4': '天体力学(理论天文学)',
+    'A007_5': '天体物理学', 'A007_6': '恒星天文学、星系天文学、宇宙学', 'A007_7': '射电天文学(无线电天文学)',
+    'A007_8': '空间天文学', 'A007_9': '太阳系', 'A007_A': '时间、历法',
+    'A008_1': '自然地理学', 'A008_2': '测绘学',
+    'A009_1': '气象工作及气象经济', 'A009_2': '气象基础科学', 'A009_3': '大气探测(气象观测)', 'A009_4': '气象要素及大气现象',
+    'A009_5': '动力气象学', 'A009_6': '天气学', 'A009_7': '天气预报', 'A009_8': '气候学', 'A009_9': '气象灾害学',
+    'A009_A': '应用气象学', 'A009_B': '气象设备仪器制造、使用', 'A009_C': '人工影响天气',
+    'A010_1': '海洋学', 'A010_2': '海洋调查技术方法与设备', 'A010_3': '区域海洋学', 'A010_4': '海洋气象学',
+    'A010_5': '海洋水文学', 'A010_6': '海洋物理学', 'A010_7': '海洋化学', 'A010_8': '海洋生物学', 'A010_9': '海洋地球物理学',
+    'A010_A': '海洋地质学、海洋地貌学', 'A010_B': '海洋资源与开发', 'A010_C': '海洋工程', 'A010_D': '海洋环境及海洋保护学',
+    'A010_E': '海洋灾害',
+    'A011_1': '地质工作及地质经济', 'A011_2': '地质灾害及灾害地质学', 'A011_3': '矿物学', 'A011_4': '岩石学',
+    'A011_5': '古生物学', 'A011_6': '历史地质学、地层学', 'A011_7': '动力地质学', 'A011_8': '构造地质学',
+    'A011_9': '地质力学', 'A011_A': '区域地质学', 'A011_B': '矿床学', 'A011_C': '地球化学', 'A011_D': '普查勘探地质学',
+    'A011_E': '勘探工程学', 'A011_F': '宇宙地质学', 'A011_G': '海洋地质学', 'A011_H': '水文地质学与工程地质学',
+    'A011_I': '环境地质学', 'A011_K': '地质勘查仪器及设备',
+    'A012_1': '一般理论', 'A012_2': '地球起源及演化', 'A012_3': '地球形状学及地球结构', 'A012_4': '地球重力学',
+    'A012_5': '大地构造物理学、岩组学(构造岩石学)', 'A012_6': '地热学、火山学', 'A012_7': '地磁学',
+    'A012_8': '地电学', 'A012_9': '水文学', 'A012_A': '空间物理', 'A012_B': '地震地球物理学', 'A012_C': '勘查地球物理学',
+    'A012_D': '海洋地球物理学', 'A012_E': '工程及水文地球物理勘探', 'A012_F': '地球物理勘测仪器',
+    'A013_1': '资源管理与利用', 'A013_2': '各种资源的开发与利用'
+}
+
 year = 2011
 collage_name = '北京大学'
 
 driver.switch_to.window(driver.window_handles[0])
 driver.switch_to.default_content()
 
-crawler('北京大学', 2011, 2011, ['A' + '{0:03d}'.format(x) for x in range(11, 14)])
+crawler('北京大学', 2011, 2011, ['A' + '{0:03d}'.format(x) for x in range(1,14)])
--- processdata.py
+++ processdata.py
@@ -1,42 +1,46 @@
+import pandas as pd
+
 headers = [
     'SrcDatabase-来源库', 'Title-题名', 'Author-作者', 'Organ-单位', 'Source-文献来源', 'Keyword-关键词', 'Summary-摘要',
     'PubTime-发表时间', 'FirstDuty-第一责任人', 'Fund-基金', 'Year-年', 'Period-期', 'PageCount-页码'
 ]
 
+#
+# with open('CNKI-636597288626875000.txt', 'br') as f:
+#     filelines = list()
+#     for l in f:
+#         filelines.append(l.decode('utf-8').rstrip())
+#
+# data = list()
+# i = -1
+#
+# while True:
+#     i = i + 1
+#     if i == len(filelines):
+#         break
+#     if filelines[i] == '':
+#         continue
+#     else:
+#         if filelines[i].find(headers[0]) >= 0:
+#             datdict = dict()
+#             initem = True
+#             j = 0
+#             while True:
+#                 if not initem:
+#                     # 已经不在 item 内
+#                     break
+#                 if filelines[i].find(headers[12]) >= 0:
+#                     # 到了该item的最后一行
+#                     initem = False
+#                 if filelines[i].find(headers[j]) >= 0:
+#                     # 找到了item的headers
+#                     datdict[headers[j]] = filelines[i].replace(headers[j] + ':', '').strip()
+#                     j = j + 1
+#                 else:
+#                     datdict[headers[j-1]] = ';'.join([datdict[headers[j-1]], filelines[i].strip()])
+#                 i = i + 1
+#             data.append(datdict)
+#             i = i - 1
+#
 
-with open('CNKI-636597288626875000.txt', 'br') as f:
-    filelines = list()
-    for l in f:
-        filelines.append(l.decode('utf-8').rstrip())
-
-data = list()
-i = -1
-
-while True:
-    i = i + 1
-    if i == len(filelines):
-        break
-    if filelines[i] == '':
-        continue
-    else:
-        if filelines[i].find(headers[0]) >= 0:
-            datdict = dict()
-            initem = True
-            j = 0
-            while True:
-                if not initem:
-                    # 已经不在 item 内
-                    break
-                if filelines[i].find(headers[11]) >= 0:
-                    # 到了该item的最后一行
-                    initem = False
-                if filelines[i].find(headers[j]) >= 0:
-                    # 找到了item的headers
-                    datdict[headers[j]] = filelines[i].replace(headers[j] + ':', '').strip()
-                    j = j + 1
-                else:
-                    datdict[headers[j-1]] = ';'.join([datdict[headers[j-1]], filelines[i].strip()])
-                i = i + 1
-            data.append(datdict)
-            i = i - 1
-
+data = pd.read_html('CNKI-636610206041782500.xls', header=0)
